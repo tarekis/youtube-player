1768033b7dc621525c2ec2dcccc4add8e5e6f174
Clemens Himmer <clemens.himmer@hotmail.com>
2017-11-23 23:38:22 +0100

    fuckbooooi that should work

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,58 +1,99 @@
-'use strict'
+'use strict';
 
-import { app, BrowserWindow } from 'electron'
-
-const isDevelopment = process.env.NODE_ENV !== 'production'
+import { app, BrowserWindow, globalShortcut } from 'electron';
 
 // Global reference to mainWindow
 // Necessary to prevent win from being garbage collected
-let mainWindow
+let mainWindow;
 
 function createMainWindow() {
-  // Construct new BrowserWindow
-  const window = new BrowserWindow()
+    // Construct new BrowserWindow
+    const window = new BrowserWindow({
+        title: 'Youtube Player',
+        titleBarStyle: 'hidden'
+    });
 
-  // Set url for `win`
-    // points to `webpack-dev-server` in development
-    // points to `index.html` in production
-  const url = isDevelopment
-    ? `http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`
-    : `file://${__dirname}/index.html`
+    window.loadURL(`file:///${__dirname}/../renderer/index.html`);
 
-  if (isDevelopment) {
-    window.webContents.openDevTools()
-  }
+    window.on('closed', () => {
+        mainWindow = null;
+    });
 
-  window.loadURL(url)
-
-  window.on('closed', () => {
-    mainWindow = null
-  })
-
-  window.webContents.on('devtools-opened', () => {
-    window.focus()
-    setImmediate(() => {
-      window.focus()
-    })
-  })
-
-  return window
+    return window;
 }
+
+app.on('browser-window-created', (_e, window) =>  {
+    window.setMenu(null);
+    window.maximize();
+});
+
+app.on('ready', () => {
+    globalShortcut.register('MediaPlayPause', () => {
+        sendCommand('MediaPlayPause');
+    });
+
+    globalShortcut.register('MediaPreviousTrack', () => {
+        sendCommand('MediaPreviousTrack');
+    });
+
+    globalShortcut.register('MediaNextTrack', () => {
+        sendCommand('MediaNextTrack');
+    });
+});
 
 // Quit application when all windows are closed
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications to stay open
-  // until the user explicitly quits
-  if (process.platform !== 'darwin') app.quit()
-})
+    // On macOS it is common for applications to stay open
+    // until the user explicitly quits
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
 
 app.on('activate', () => {
-  // On macOS it is common to re-create a window
-  // even after all windows have been closed
-  if (mainWindow === null) mainWindow = createMainWindow()
-})
+    // On macOS it is common to re-create a window
+    // even after all windows have been closed
+    if (mainWindow === null) {
+        mainWindow = createMainWindow();
+    }
+});
 
 // Create main BrowserWindow when electron is ready
 app.on('ready', () => {
-  mainWindow = createMainWindow()
-})
+    mainWindow = createMainWindow();
+});
+
+function sendCommand(command: string) {
+    mainWindow.webContents.send('control', command);
+}
+
+// Linux Media Key bindings
+function registerBindings(desktopEnv, session) {
+    session.getInterface(`org.${desktopEnv}.SettingsDaemon`,
+    `/org/${desktopEnv}/SettingsDaemon/MediaKeys`,
+    `org.${desktopEnv}.SettingsDaemon.MediaKeys`, (err, iface) => {
+        if (!err) {
+            iface.on('MediaPlayerKeyPressed', (_n, keyName) => {
+                switch (keyName) {
+                    case 'Next': sendCommand('MediaPlayPause'); return;
+                    case 'Previous': sendCommand('MediaPreviousTrack'); return;
+                    case 'Play': sendCommand('MediaNextTrack'); return;
+                    default: return;
+                }
+            });
+            iface.GrabMediaPlayerKeys(0, `org.${desktopEnv}.SettingsDaemon.MediaKeys`); // eslint-disable-line
+        }
+    });
+}
+
+try {
+    const DBus = require('dbus');
+
+    const dbus = new DBus();
+    const session = dbus.getBus('session');
+
+    registerBindings('gnome', session);
+    registerBindings('mate', session);
+} catch (e) {
+    // do nothing.
+}
